29243cde92d567a4983183af837ac7379342d246
yacchi3298 <yacchi1854@icloud.com>
2023-02-12 12:50:07 +0000

    課題提出１回目

--- memo.rb
+++ memo.rb
@@ -11,15 +11,23 @@ if memo_type == 1
   file_name = gets.chomp
   puts "メモを入力"
   puts "入力後、Ctrl + D で保存"
-  memo = STDIN.read
+  memo = STDIN.read.chomp
   
-  CSV.open('#{file_name}.csv','w') do |csv|
+  CSV.open("#{file_name}.csv","w") do |csv|
   csv << ["#{memo}"]
   end
   
 elsif memo_type == 2
-  puts "既存のメモを編集します。"
-else
-  puts "数字を選択してください。"
+  puts "既存のメモを編集します。拡張子を除いたファイル名を入力してください。"
+  file_name = gets.chomp
+  puts "メモを入力"
+  puts "入力後、Ctrl + D で保存"
+  memo = STDIN.read.chomp
+  
+  CSV.open("#{file_name}.csv","a") do |csv|
+  csv << ["#{memo}"]
+
+  end
+
   
 end
